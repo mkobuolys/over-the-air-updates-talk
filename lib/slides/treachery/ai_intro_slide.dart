180a3d623afc6ee1839da04f29843a008d068f9a
Mangirdas Kazlauskas <mangirdas@billo.app>
2025-03-29 23:07:51 +0200

    Update web config, add AI intro slide

---
+++ lib/slides/treachery/ai_intro_slide.dart
@@ -0,0 +1,66 @@
+import 'package:confetti/confetti.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_deck/flutter_deck.dart';
+
+class AiIntroSlide extends FlutterDeckSlideWidget {
+  const AiIntroSlide()
+      : super(
+          configuration: const FlutterDeckSlideConfiguration(
+            route: '/ai-intro',
+            title: 'AI',
+          ),
+        );
+
+  @override
+  FlutterDeckSlide build(BuildContext context) {
+    return FlutterDeckSlide.blank(builder: (context) => const _Content());
+  }
+}
+
+class _Content extends StatefulWidget {
+  const _Content();
+
+  @override
+  State<_Content> createState() => _ContentState();
+}
+
+class _ContentState extends State<_Content> {
+  late final ConfettiController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = ConfettiController(duration: const Duration(seconds: 2));
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    _controller.play();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ConfettiWidget(
+        blastDirectionality: BlastDirectionality.explosive,
+        emissionFrequency: 0.05,
+        minBlastForce: 15,
+        confettiController: _controller,
+        child: Text(
+          'AI',
+          style: FlutterDeckBigFactSlideTheme.of(context).titleTextStyle,
+        ),
+      ),
+    );
+  }
+}
--- lib/slides/treachery/treachery.dart
+++ lib/slides/treachery/treachery.dart
@@ -1,5 +1,7 @@
+import 'ai_intro_slide.dart';
 import 'treachery_section_slide.dart';
 
 const treacherySlides = [
   TreacherySectionSlide(),
+  AiIntroSlide(),
 ];
