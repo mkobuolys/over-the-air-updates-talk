a688f54622c331188c051c9f4a74bbb10ea5d87c
Mangirdas Kazlauskas <mangirdas@billo.app>
2025-11-19 22:36:15 +0200

    feat: remove feature flag slides

--- lib/slides/configuration/configuration.dart
+++ lib/slides/configuration/configuration.dart
@@ -1,10 +1,7 @@
 import 'configuration_section_slide.dart';
 import 'crowdin_slide.dart';
-import 'feature_flags_overview_slide.dart';
-import 'feature_flags_slide.dart';
 import 'firebase_remote_config_slide.dart';
 import 'intercom_help_center_slide.dart';
-import 'personalized_config_slide.dart';
 import 'revenue_cat_paywall_slide.dart';
 
 const configurationSlides = [
@@ -13,7 +10,4 @@ const configurationSlides = [
   IntercomHelpCenterSlide(),
   RevenueCatPaywallSlide(),
   FirebaseRemoteConfigSlide(),
-  FeatureFlagsOverviewSlide(),
-  FeatureFlagsSlide(),
-  PersonalizedConfigSlide(),
 ];
--- packages/notes_app/lib/notes_app.dart
+++
@@ -1,1 +0,0 @@
-export 'src/app.dart';
