f44cbb7fd562701a63294f9edbd2c93f5974f443
Mangirdas Kazlauskas <mangirdas@billo.app>
2025-11-29 16:50:52 +0200

    feat: remove slides, some tests

--- lib/slides/create_ui/create_ui.dart
+++ lib/slides/create_ui/create_ui.dart
@@ -1,8 +1,6 @@
 import 'ai_intro_slide.dart';
-import 'design_trends_slide.dart';
 import 'experiences_section_slide.dart';
 import 'flutter_genui_architecture_slide.dart';
-import 'flutter_genui_project_slide.dart';
 import 'generative_ui_overview_slide.dart';
 import 'generative_ui_scheme_slide.dart';
 import 'slide_content_generation_slide.dart';
@@ -11,11 +9,9 @@ import 'travel_app_demo_slide.dart';
 const createUiSlides = [
   CreateUiSectionSlide(),
   AiIntroSlide(),
-  DesignTrendsSlide(),
   SlideContentGenerationSlide(),
   GenerativeUiOverviewSlide(),
   GenerativeUiSchemeSlide(),
-  FlutterGenuiProjectSlide(),
   FlutterGenuiArchitectureSlide(),
   TravelAppDemoSlide(),
 ];
